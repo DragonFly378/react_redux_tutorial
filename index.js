e11059b22d2adce087806265c3d67488f191e5cf
DargonFly378 <hafichever@gmail.com>
2022-05-24 15:16:19 +0700

    materi store

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
+/* create actions */
 function buyCake() {
   return {
     type: BUY_CAKE,
@@ -9,7 +13,6 @@ function buyCake() {
 
 /* make reducer */
 /** (previousState, action) => newState  */
-
 const initialState = {
   numOfCakes: 10,
 };
@@ -21,8 +24,18 @@ const reducer = (state = initialState, action) => {
         ...state, // untuk tetap mengambil semua isi dari state
         numOfCakes: state.numOfCakes - 1,
       };
-
     default:
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated State", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
