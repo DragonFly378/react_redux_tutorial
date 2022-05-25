0da1fd4bb8a40773584714bcdfa0407a7b7acfc6
DargonFly378 <hafichever@gmail.com>
2022-05-25 11:44:36 +0700

    materi combineReducer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICEREAM";
@@ -21,18 +22,27 @@ function buyIceCream() {
 
 /* make reducer */
 /** (previousState, action) => newState  */
-const initialState = {
+initialCakeState = {
   numOfCakes: 10,
+};
+initialIceCreamState = {
   numOfIceCream: 20,
 };
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state, // untuk tetap mengambil semua isi dari state
         numOfCakes: state.numOfCakes - 1,
       };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
     case BUY_ICECREAM:
       return {
         ...state,
@@ -43,7 +53,12 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store = createStore(rootReducer);
 console.log("initial state", store.getState());
 
 const unsubscribe = store.subscribe(() =>
