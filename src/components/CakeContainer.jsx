26408033d10e953063d3468f7f6e96225cbf202d
DargonFly378 <hafichever@gmail.com>
2022-05-26 12:28:28 +0700

    materi connect

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,14 +1,30 @@
 import React from "react";
 
-const CakeContainer = () => {
+import { connect } from "react-redux";
+import { buyCake } from "../store";
+
+const CakeContainer = ({ buyCake, numOfCakes }) => {
   return (
     <>
       <div>
-        <h2>Number of Cakes</h2>
-        <button>Buy Cake</button>
+        <h2>Number of Cakes - {numOfCakes}</h2>
+        <button onClick={buyCake}>Buy Cake</button>
       </div>
     </>
   );
 };
 
-export default CakeContainer;
+// memanggil state dari redux
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
