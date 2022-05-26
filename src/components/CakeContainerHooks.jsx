05b3b8e1894ffc3a0b56c9fd7e10b2c201ee3e6e
DargonFly378 <hafichever@gmail.com>
2022-05-26 13:48:57 +0700

    materi dispatch hooks

--- src/components/CakeContainerHooks.jsx
+++ src/components/CakeContainerHooks.jsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../store";
 
 const CakeContainerHooks = () => {
+  const dispatch = useDispatch();
   const numOfCakes = useSelector((state) => state.numOfCakes);
   return (
     <>
       <div>
         <h2>hooks-Number of Cakes - {numOfCakes}</h2>
-        <button>Buy Cake</button>
+        <button
+          onClick={() => {
+            dispatch(buyCake());
+          }}
+        >
+          Buy Cake
+        </button>
       </div>
     </>
   );
